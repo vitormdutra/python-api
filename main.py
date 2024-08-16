dd3b3fcb772f4a6b708672617d12b2b19c54c365
Vitor Marcus Dutra <ntw1942@gmail.com>
2024-08-16 19:56:59 -0300

    feat: weather search, cache information and remove old endpoints

--- main.py
+++ main.py
@@ -1,40 +1,40 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Query, Header
+from fastapi.responses import JSONResponse
+import requests
 import uvicorn
+import logging
+from cachetools import cached, TTLCache
 
-static_string = "Initial Text"
+logging.basicConfig(level=logging.INFO)
+cache = TTLCache(maxsize=100, ttl=300)
+OPENWEATHERMAP_API_KEY = "Information goes here"
+
 app = FastAPI()
 
+@cached(cache)
+def fetch_weather_data(url):
+    try:
+        response = requests.get(url, timeout=5)
+        response.raise_for_status()
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        logging.error(f"Error fetching weather data: {e}")
+        return None
 
-@app.get("/get-message")
-def hello(name: str):
-    return {'Message': "Congrats " + name + '! This is your first API!'}
+@app.get("/weather", summary="Get weather information", description="Retrieve weather data for a specific city and country.")
+def get_weather(api_key: str = Header(...), city: str = Query(..., description="Name of the city"), country: str = Query(..., description="Country code (e.g., 'BR' for Brazil)")):
+    if api_key != "Define a key for API":
+        raise HTTPException(status_code=403, detail="Invalid API Key")
+    logging.info(f"Receiving request for weather forecast for {city}, {country}")
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city},{country}&appid={OPENWEATHERMAP_API_KEY}&units=metric"
 
+    weather = fetch_weather_data(url)
+    if not weather:
+        logging.error(f"Failed to fetch weather data for {city}, {country}")
+        raise HTTPException(status_code=502, detail="Failed to fetch weather data")
 
-@app.post("/add")
-async def add_text(text: str):
-    global static_string
-    static_string += text
-    return {"Message": "Text added", "current_string": static_string}
-
-
-@app.put("/change")
-async def change_text(new_text: str):
-    global static_string
-    static_string = new_text
-    return {"Message": "Text changed", "current_string": static_string}
-
-
-@app.delete("/delete")
-async def delete_text():
-    global static_string
-    static_string = ""
-    return {"Message": "Text deleted"}
-
-@app.get("/get-all")
-async def all_text():
-    global static_string
-    return static_string
-
+    logging.info(f"Weather data successfully retrieved for {city}, {country}")
+    return JSONResponse(content=weather)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
